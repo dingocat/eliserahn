58f3d80e6080437d08bc993d5736a67cdfa5f560
dingocat <wizomagicland@gmail.com>
2014-06-06 15:25:22 -0400

    Adding media section, styling update with drop shadows and color.≈

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+$(document).ready(function() {
+    $("#image1").fadeIn("slow");
+    $("#image2").fadeOut("fast");
+    $("#image3").fadeOut("fast");
 
+    $("#thumb1").click(function() {
+        $("#image1").fadeIn("slow");
+        $("#image2").fadeOut("fast");
+        $("#image3").fadeOut("fast");
+    });
+    $("#thumb2").click(function() {
+        $("#image1").fadeOut("fast");
+        $("#image2").fadeIn("slow");
+        $("#image3").fadeOut("fast");
+    });
+    $("#thumb3").click(function() {
+        $("#image1").fadeOut("fast");
+        $("#image2").fadeOut("fast");
+        $("#image3").fadeIn("slow");
+    });
+});
